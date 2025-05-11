e02b5c6740bc90f6215620ac2d4e96fd01cf2296
Artauthr <arthurthkng@gmail.com>
2025-05-11 13:02:43 +0400

    settings

--- html/src/main/java/art/emu/gwt/GwtLauncher.java
+++ html/src/main/java/art/emu/gwt/GwtLauncher.java
@@ -10,13 +10,14 @@ public class GwtLauncher extends GwtApplication {
         @Override
         public GwtApplicationConfiguration getConfig () {
             // Resizable application, uses available space in browser with no padding:
-            GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(true);
+            GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(false);
             cfg.padVertical = 0;
             cfg.padHorizontal = 0;
-            return cfg;
+//            cfg.s
+//            return cfg;
             // If you want a fixed size application, comment out the above resizable section,
-            // and uncomment below:
-            //return new GwtApplicationConfiguration(640, 480);
+            // and uncomment below:configuration.setWindowedMode(768, 672);
+            return new GwtApplicationConfiguration(768, 672);
         }
 
         @Override
